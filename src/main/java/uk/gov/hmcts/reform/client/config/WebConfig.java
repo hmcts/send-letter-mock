da31e14304f9efdb104bcd288f460ef8d23cae27
debolina-seal <debolina.seal@gmail.com>
2026-05-14 12:11:14 +0100

    spring boot upgrade

--- src/main/java/uk/gov/hmcts/reform/client/config/WebConfig.java
+++ src/main/java/uk/gov/hmcts/reform/client/config/WebConfig.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.client.config;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.core.JacksonException;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
@@ -28,7 +28,7 @@ public class WebConfig implements WebMvcConfigurer {
         public Map<String, String> convert(String input) {
             try {
                 return objectMapper.readValue(input, new TypeReference<Map<String, String>>() {});
-            } catch (JsonProcessingException e) {
+            } catch (JacksonException e) {
                 throw new IllegalArgumentException("Failed to convert json to map", e);
             }
         }
