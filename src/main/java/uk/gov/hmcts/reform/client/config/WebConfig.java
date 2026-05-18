10fdea0c46a2a62d4617210b9d00c0dffe5df14b
debolina-seal <debolina.seal@gmail.com>
2026-05-18 11:18:49 +0100

    configuration class to Provides com.fasterxml.jackson.databind.ObjectMapper so Feign/Send Letter client can deserialize responses.

--- src/main/java/uk/gov/hmcts/reform/client/config/WebConfig.java
+++ src/main/java/uk/gov/hmcts/reform/client/config/WebConfig.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.client.config;
 
-import tools.jackson.core.JacksonException;
-import tools.jackson.core.type.TypeReference;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
